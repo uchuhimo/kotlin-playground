c97c68fa1669761a3f1868b1043ee957d9583b9b
uchuhimo <uchuhimo@outlook.com>
2017-05-04 02:30:18 +0800

    play: use extend pattern to simulate type class

--- lang/src/main/kotlin/com/uchuhimo/typeclass/extend/UseShow.kt
+++ lang/src/main/kotlin/com/uchuhimo/typeclass/extend/UseShow.kt
@@ -4,44 +4,65 @@ import com.uchuhimo.typeclass.Base
 import com.uchuhimo.typeclass.Derived
 import com.uchuhimo.typeclass.Show
 
-fun <A> A.log(s: Show<A>) {
-    println(s.show(this))
+interface Loggable {
+    fun show(): String
+
+    fun log() {
+        println(show())
+    }
 }
 
-fun <A : Show<A>> A.log() {
-    this.log(this)
+class LoggableClass : Loggable {
+    override fun show(): String = "loggable"
 }
+
+infix fun <T> T.extend(s: Show<T>): Loggable = object : Loggable {
+    override fun show(): String = s.show(this@extend)
+}
+
+val showString: Show<String> get() = object : Show<String> {
+    override fun show(f: String): String = f
+}
+
+val String.extendShow get() = this extend showString
 
 fun String.log() {
-    this.log(object : Show<String> {
-        override fun show(f: String): String = f
-    })
+    this.extendShow.log()
 }
+
+val showBaseList: Show<List<Base>> get() = object : Show<List<Base>> {
+    override fun show(f: List<Base>): String = "base list"
+}
+
+val List<Base>.extendShow
+    @JvmName("extendShowForBaseList")
+    get() = this extend showBaseList
 
 @JvmName("logForBaseList")
 fun List<Base>.log() {
-    this.log(object : Show<List<Base>> {
-        override fun show(f: List<Base>): String = "base list"
-    })
+    this.extendShow.log()
 }
+
+val showDerivedList: Show<List<Derived>> get() = object : Show<List<Derived>> {
+    override fun show(f: List<Derived>): String = "derived list"
+}
+
+val List<Derived>.extendShow
+    @JvmName("extendShowForDerivedList")
+    get() = this extend showDerivedList
 
 @JvmName("logForDerivedList")
 fun List<Derived>.log() {
-    this.log(object : Show<List<Derived>> {
-        override fun show(f: List<Derived>): String = "base list"
-    })
-}
-
-fun Derived.log() {
-    this.log(object : Show<Derived> {
-        override fun show(f: Derived): String = f.show(f) + " derived"
-    })
+    this.extendShow.log()
 }
 
 fun main(args: Array<String>) {
+    "test".extend(showString).log()
+    "test".extendShow.log()
     "test".log()
+    listOf(Base(), Base()).extendShow.log()
     listOf(Base(), Base()).log()
+    listOf(Derived(), Derived()).extendShow.log()
     listOf(Derived(), Derived()).log()
-    Base().log()
-    Derived().log()
+    LoggableClass().log()
 }
