1b3f706f1173222faef722ee75ab91bf3ecefeae
uchuhimo <uchuhimo@outlook.com>
2017-05-27 18:15:10 +0800

    play: add block test

---
+++ lang/src/main/kotlin/com/uchuhimo/block/Block.kt
@@ -0,0 +1,17 @@
+package com.uchuhimo.block
+
+fun main(args: Array<String>) {
+    val (outerA, _) = run {
+        val a = 1
+        Pair(a, 11)
+    }
+    println(outerA)
+    val outerB = fun(): Int {
+        val b = 2
+        return b
+    }()
+    println(outerB)
+    run { // doc here
+        println("just a block")
+    }
+}
