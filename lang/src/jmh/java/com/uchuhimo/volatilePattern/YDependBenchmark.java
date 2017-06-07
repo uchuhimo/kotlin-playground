19d822b4443590a604140a298e1dfb1bb1f25a2c
uchuhimo <uchuhimo@outlook.com>
2017-06-08 05:34:32 +0800

    refine: define jmh annotation on class

--- lang/src/jmh/java/com/uchuhimo/volatilePattern/YDependBenchmark.java
+++ lang/src/jmh/java/com/uchuhimo/volatilePattern/YDependBenchmark.java
@@ -3,16 +3,19 @@ package com.uchuhimo.volatilePattern;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class YDependBenchmark {
   @Benchmark
-  @BenchmarkMode(Mode.Throughput)
   public void testOnce() {
     new YDepend().testOnce();
   }
 
   @Benchmark
-  @BenchmarkMode(Mode.Throughput)
   public void testAtLeastOnce() {
     new YDepend().testAtLeastOnce();
   }
