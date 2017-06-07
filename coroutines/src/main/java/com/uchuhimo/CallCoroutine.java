6b97021fe938d432d5a65dd42b73e29d03592cee
uchuhimo <uchuhimo@outlook.com>
2017-06-08 01:38:35 +0800

    play: add coroutine test

---
+++ coroutines/src/main/java/com/uchuhimo/CallCoroutine.java
@@ -0,0 +1,29 @@
+package com.uchuhimo;
+
+import kotlin.Unit;
+import kotlin.coroutines.experimental.EmptyCoroutineContext;
+import kotlin.coroutines.experimental.intrinsics.IntrinsicsKt;
+import kotlin.jvm.internal.Intrinsics;
+import kotlinx.coroutines.experimental.BuildersKt;
+
+public class CallCoroutine {
+  public static void main(String[] args) {
+    try {
+      BuildersKt.runBlocking(EmptyCoroutineContext.INSTANCE, (coroutineScope, continuation) -> {
+        CoroutineTestKt.sayHello(continuation);
+        return Unit.INSTANCE;
+      });
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    CoroutineTestKt.executeBlocking(() -> {
+      System.out.println("execute normal");
+      return Unit.INSTANCE;
+    });
+    CoroutineTestKt.executeBlocking(continuation -> {
+      System.out.println("execute suspend");
+      continuation.resume(Unit.INSTANCE);
+      return IntrinsicsKt.getCOROUTINE_SUSPENDED();
+    });
+  }
+}
---
+++ coroutines/src/main/kotlin/com/uchuhimo/CoroutineTest.kt
@@ -0,0 +1,138 @@
+package com.uchuhimo
+
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.channels.*
+import kotlinx.coroutines.experimental.selects.select
+import kotlinx.coroutines.experimental.sync.Mutex
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.coroutines.experimental.suspendCoroutine
+import kotlin.system.measureTimeMillis
+
+val scheduledExecutor = ScheduledThreadPoolExecutor(1, { r -> Thread(r).apply { isDaemon = true } })
+
+object CommonPoolWithDelay : CoroutineDispatcher(), Delay {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        CommonPool.dispatch(context, block)
+    }
+
+    override fun scheduleResumeAfterDelay(time: Long, unit: TimeUnit, continuation: CancellableContinuation<Unit>) {
+        println("${Thread.currentThread().name}: in CommonPoolWithDelay")
+        scheduledExecutor.schedule({ continuation.resume(Unit) }, time, unit)
+    }
+}
+
+suspend fun sayHello() {
+    return suspendCoroutine { cont ->
+        println("${Thread.currentThread().name}: hello")
+        cont.resume(Unit)
+    }
+}
+
+fun executeBlocking(block: suspend () -> Unit) {
+    runBlocking(block = { block() })
+}
+
+fun executeBlocking(block: () -> Unit) {
+    block()
+}
+
+fun main1(args: Array<String>) = runBlocking<Unit> {
+    println("${Thread.currentThread().name}: run")
+    val job: Job = launch(CommonPoolWithDelay) {
+        delay(1000L)
+        println("${Thread.currentThread().name}: delay")
+    }
+    sayHello()
+    job.join()
+    yield()
+    val launch = launch(context) {
+        try {
+            run(NonCancellable) {
+                delay(1000L)
+            }
+        } catch (e: CancellationException) {
+            println("cancel")
+        }
+    }
+    produce<Int>(CommonPool) {
+        for (x in 1..5) send(x * x)
+    }
+    delay(500L)
+    launch.cancel()
+}
+
+var counter = 0
+val counterContext = newSingleThreadContext("CounterContext")
+val mutex = Mutex()
+
+suspend inline fun Mutex.withLock(action: () -> Unit) {
+    lock()
+    try {
+        action()
+    } finally {
+        unlock()
+    }
+}
+
+suspend fun massiveRun(context: CoroutineContext, action: suspend () -> Unit) {
+    val n = 10_000 // number of coroutines to launch
+    val k = 10_000 // times an action is repeated by each coroutine
+    val time = measureTimeMillis {
+        val jobs = List(n) {
+            launch(context) {
+                repeat(k) { action() }
+            }
+        }
+        jobs.forEach { it.join() }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
+
+fun main2(args: Array<String>) = runBlocking<Unit> {
+    massiveRun(CommonPool) {
+        mutex.withLock {
+            counter++
+        }
+    }
+    println("Counter = ${counter}")
+}
+
+// Message types for counterActor
+sealed class CounterMsg
+object IncCounter : CounterMsg() // one-way message to increment counter
+class GetCounter(val response: SendChannel<Int>) : CounterMsg() // a request with reply
+
+// This function launches a new counter actor
+fun counterActor() = actor<CounterMsg>(CommonPool) {
+    var counter = 0 // actor state
+    for (msg in channel) { // iterate over incoming messages
+        when (msg) {
+            is IncCounter -> counter++
+            is GetCounter -> msg.response.send(counter)
+        }
+    }
+}
+
+fun main(args: Array<String>) = runBlocking<Unit> {
+    val counter = counterActor() // create the actor
+    massiveRun(CommonPool) {
+        counter.send(IncCounter)
+    }
+    val response = Channel<Int>()
+    counter.send(GetCounter(response))
+    println("Counter = ${response.receive()}")
+    counter.close() // shutdown the actor
+}
+
+suspend fun selectFizzBuzz(fizz: ReceiveChannel<String>, buzz: ReceiveChannel<String>) {
+    select<Unit> { // <Unit> means that this select expression does not produce any result
+        fizz.onReceive { value ->  // this is the first select clause
+            println("fizz -> '$value'")
+        }
+        buzz.onReceive { value ->  // this is the second select clause
+            println("buzz -> '$value'")
+        }
+    }
+}
