eb7a76d0c4d63aff41027b9dbcbc8d7e8268ade8
uchuhimo <uchuhimo@outlook.com>
2017-06-18 22:10:40 +0800

    play: enrich BiMap, support biway transform between MutableBiMap and GuavaBiMap

--- lang/src/main/kotlin/com/uchuhimo/collections/BiMaps.kt
+++ lang/src/main/kotlin/com/uchuhimo/collections/BiMaps.kt
@@ -12,6 +12,38 @@ interface MutableBiMap<K, V> : MutableMap<K, V>, BiMap<K, V> {
     override val inverse: MutableBiMap<V, K>
 
     override val values: MutableSet<V>
+
+    /**
+     * Associates the specified [value] with the specified [key] in the bimap.
+     *
+     * The bimap throws [IllegalArgumentException] if the given value is already
+     * bound to a different key in it. The bimap will remain unmodified in this
+     * event. To avoid this exception, call [forcePut] instead.
+     *
+     * @return the previous value associated with the key, or `null` if the key
+     *         was not present in the bimap.
+     */
+    override fun put(key: K, value: V): V?
+
+    /**
+     * An alternate form of [put] that silently removes any existing entry
+     * with the value [value] before proceeding with the [put] operation.
+     *
+     * If the bimap previously contained the provided key-value
+     * mapping, this method has no effect.
+     *
+     * Note that a successful call to this method could cause the size of the
+     * bimap to increase by one, stay the same, or even decrease by one.
+     *
+     * **Warning**: If an existing entry with this value is removed, the key
+     * for that entry is discarded and not returned.
+     *
+     * @param key the key with which the specified value is to be associated.
+     * @param value the value to be associated with the specified key.
+     * @return the value which was previously associated with the key, or `null`
+     *         if there was no previous entry.
+     */
+    fun forcePut(key: K, value: V): V?
 }
 
 private class BiMapImpl<K, V> private constructor(delegate: Map<K, V>) :
@@ -91,6 +123,10 @@ private class MutableBiMapImpl<K, V> private constructor(private val delegate: M
         return delegate.put(key, value)?.also { oldValue -> inverseDelegate.remove(oldValue) }
     }
 
+    override fun forcePut(key: K, value: V): V? {
+        TODO("not implemented")
+    }
+
     override fun putAll(from: Map<out K, V>) {
         from.forEach { key, value -> this[key] = value }
     }
@@ -106,21 +142,73 @@ private class MutableBiMapImpl<K, V> private constructor(private val delegate: M
 
 typealias GuavaBiMap<K, V> = com.google.common.collect.BiMap<K, V>
 
-class GuavaBiMapWrapper<K, V>(private val delegate: GuavaBiMap<K, V>) :
+class MutableBiMapWrapper<K, V>(internal val delegate: GuavaBiMap<K, V>) :
         MutableBiMap<K, V>, MutableMap<K, V> by delegate {
     override val inverse: MutableBiMap<V, K> = InverseWrapper(this)
 
     override val values: MutableSet<V> = delegate.values
 
+    override fun forcePut(key: K, value: V): V? = delegate.forcePut(key, value)
+
     companion object {
-        private class InverseWrapper<K, V>(wrapper: GuavaBiMapWrapper<V, K>) :
+        private class InverseWrapper<K, V>(private val wrapper: MutableBiMapWrapper<V, K>) :
                 MutableBiMap<K, V>, MutableMap<K, V> by wrapper.delegate.inverse() {
             override val inverse: MutableBiMap<V, K> = wrapper
 
             override val values: MutableSet<V> = wrapper.delegate.inverse().values
+
+            override fun forcePut(key: K, value: V): V? =
+                    wrapper.delegate.inverse().forcePut(key, value)
         }
     }
 }
+
+class GuavaBiMapWrapper<K, V>(internal val delegate: MutableBiMap<K, V>) :
+        GuavaBiMap<K, V> {
+    override fun putAll(from: Map<out K, V>) = delegate.putAll(from)
+
+    override val size: Int get() = delegate.size
+
+    override fun containsKey(key: K): Boolean = delegate.containsKey(key)
+
+    override fun get(key: K): V? = delegate.get(key)
+
+    override fun remove(key: K): V? = delegate.remove(key)
+
+    override val values: MutableSet<V> get() = delegate.values
+
+    override fun containsValue(value: V): Boolean = delegate.containsValue(value)
+
+    override fun isEmpty(): Boolean = delegate.isEmpty()
+
+    override fun forcePut(key: K?, value: V?): V? = delegate.forcePut(key!!, value!!)
+
+    override val entries: MutableSet<MutableMap.MutableEntry<K, V>> get() = delegate.entries
+
+    override val keys: MutableSet<K> get() = delegate.keys
+
+    override fun put(key: K?, value: V?): V? = delegate.put(key!!, value!!)
+
+    private val _inverse: GuavaBiMap<V, K> = delegate.inverse.asGuavaBiMap()
+
+    override fun inverse(): GuavaBiMap<V, K> = _inverse
+
+    override fun clear() = delegate.clear()
+}
+
+fun <K, V> MutableBiMap<K, V>.asGuavaBiMap(): GuavaBiMap<K, V> =
+        if (this is MutableBiMapWrapper) {
+            delegate
+        } else {
+            GuavaBiMapWrapper(this)
+        }
+
+fun <K, V> GuavaBiMap<K, V>.asMutableBiMap(): MutableBiMap<K, V> =
+        if (this is GuavaBiMapWrapper) {
+            delegate
+        } else {
+            MutableBiMapWrapper(this)
+        }
 
 private val emptyBiMap = BiMapImpl<Any?, Any?>(emptyMap(), emptyMap())
 
@@ -141,7 +229,7 @@ fun <K, V> biMapOf(pair: Pair<K, V>): Map<K, V> =
         BiMapImpl(mapOf(pair), mapOf(pair.second to pair.first))
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun <K, V> mutableBiMapOf(): MutableBiMap<K, V> = GuavaBiMapWrapper(HashBiMap.create())
+inline fun <K, V> mutableBiMapOf(): MutableBiMap<K, V> = HashBiMap.create<K, V>().asMutableBiMap()
 
 fun <K, V> mutableBiMapOf(vararg pairs: Pair<K, V>): MutableBiMap<K, V>
-        = GuavaBiMapWrapper<K, V>(HashBiMap.create(pairs.size)).apply { putAll(pairs) }
+        = HashBiMap.create<K, V>(pairs.size).asMutableBiMap().apply { putAll(pairs) }
