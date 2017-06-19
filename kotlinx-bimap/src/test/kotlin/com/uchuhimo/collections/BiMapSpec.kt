318a40ef432c8bb936ce2cfaeb29a5ed6c4cdb8b
uchuhimo <uchuhimo@outlook.com>
2017-06-20 02:49:01 +0800

    test(bimap): refine spec of BiMap and MutableBiMap

--- kotlinx-bimap/src/test/kotlin/com/uchuhimo/collections/BiMapSpec.kt
+++ kotlinx-bimap/src/test/kotlin/com/uchuhimo/collections/BiMapSpec.kt
@@ -1,9 +1,8 @@
 package com.uchuhimo.collections
 
-import com.natpryce.hamkrest.and
-import com.natpryce.hamkrest.assertion.assert
+import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import com.natpryce.hamkrest.hasElement
+import com.natpryce.hamkrest.isIn
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -14,13 +13,13 @@ object BiMapSpec : Spek({
         val emptyBiMap = emptyBiMap<Int, String>()
 
         it("should contain 0 element") {
-            assert.that(emptyBiMap.isEmpty(), equalTo(true))
+            assertThat(emptyBiMap.isEmpty(), equalTo(true))
         }
 
         on("inverse") {
             val inverseBiMap = emptyBiMap.inverse
             it("should contain 0 element") {
-                assert.that(inverseBiMap.isEmpty(), equalTo(true))
+                assertThat(inverseBiMap.isEmpty(), equalTo(true))
             }
         }
     }
@@ -29,7 +28,7 @@ object BiMapSpec : Spek({
         val biMap = biMapOf<Int, String>()
 
         it("should contain 0 element") {
-            assert.that(biMap.isEmpty(), equalTo(true))
+            assertThat(biMap.isEmpty(), equalTo(true))
         }
     }
 
@@ -37,13 +36,13 @@ object BiMapSpec : Spek({
         val biMap = biMapOf(1 to "1")
 
         it("should contain 1 element") {
-            assert.that(biMap.size, equalTo(1))
+            assertThat(biMap.size, equalTo(1))
         }
         it("should contain given key") {
-            assert.that(biMap.containsKey(1), equalTo(true))
+            assertThat(biMap.containsKey(1), equalTo(true))
         }
         it("should contain given value") {
-            assert.that(biMap.containsValue("1"), equalTo(true))
+            assertThat(biMap.containsValue("1"), equalTo(true))
         }
     }
 
@@ -51,21 +50,25 @@ object BiMapSpec : Spek({
         val biMap = biMapOf(1 to "1", 2 to "2", 3 to "3")
 
         it("should contain given number of elements") {
-            assert.that(biMap.size, equalTo(3))
+            assertThat(biMap.size, equalTo(3))
         }
         it("should contain given keys") {
-            assert.that(biMap.keys, hasElement(1) and hasElement(2) and hasElement(3))
+            assertThat(1, isIn(biMap.keys))
+            assertThat(2, isIn(biMap.keys))
+            assertThat(3, isIn(biMap.keys))
         }
         it("should contain given values") {
-            assert.that(biMap.values, hasElement("1") and hasElement("2") and hasElement("3"))
+            assertThat("1", isIn(biMap.values))
+            assertThat("2", isIn(biMap.values))
+            assertThat("3", isIn(biMap.values))
         }
         it("should map from key to value as expected") {
-            assert.that(biMap[1], equalTo("1"))
+            assertThat(biMap[1], equalTo("1"))
         }
         on("inverse") {
             val inverseBiMap = biMap.inverse
             it("should map from key to value as expected") {
-                assert.that(inverseBiMap["1"], equalTo(1))
+                assertThat(inverseBiMap["1"], equalTo(1))
             }
         }
     }
